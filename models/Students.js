dab763de003eaf2da3c7185d53a9bb8293b07a24
Gary Bolin <garybolin@ivytech.edu>
2026-05-04 01:41:05 -0400

    refined teacher and student model(added email and role)

--- models/Students.js
+++ models/Students.js
@@ -3,8 +3,11 @@ const db = require("../db")
 const Student = db.model("Student",{
     firstName: String,
     lastName: String,
+    email: {type: String, unique:true, lowercase:true},
     loginName: {type: String, unique: true},
-    password: String
+    password: String,
+    role: {type: String, default: "student"}
+
 })
 
 module.exports = Student;
--- models/teacher.js
+++ models/teacher.js
@@ -3,8 +3,10 @@ const db = require("../db")
 const Teacher = db.model("Teacher",{
     firstName: String,
     lastName: String,
+    email: {type: String, unique:true, lowercase:true},
     loginName: {type: String, unique: true},
-    password: String
+    password: String,
+    role: {type: String, default: "teacher"}
 })
 
 module.exports = Teacher;
