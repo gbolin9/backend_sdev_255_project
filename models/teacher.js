f14ad1a4ef4ae1889754391c27370a496625dd79
Gary Bolin <garybolin@ivytech.edu>
2026-05-04 18:44:56 -0400

    fix

--- models/teacher.js
+++ models/teacher.js
@@ -1,18 +1,17 @@
-const db = require("../db")
+const db = require("../db");
 
-const Teacher = db.model("Teacher",{
-    firstName: String,
-    lastName: String,
-    teacherID: Number,
-    email: {type: String, unique:true, lowercase:true},
-    teacherID: { 
-    type: Number, 
-    unique: true, 
+const Teacher = db.model("Teacher", {
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true, lowercase: true },
+  teacherID: {
+    type: Number,
+    unique: true,
     default: () => Math.floor(100000 + Math.random() * 900000) // Generates 6-digit ID
   },
-    loginName: {type: String, unique: true},
-    password: String,
-    role: {type: String, default: "teacher"}
-})
+  loginName: { type: String, unique: true },
+  password: { type: String, required: true }, 
+  role: { type: String, default: "teacher" }
+});
 
 module.exports = Teacher;
